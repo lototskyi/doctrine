0f3c23b855e672d1855ea3d326a06e8c64e81a65
alex <o.lototskyi@gmail.com>
2018-10-26 17:11:15 +0300

    schema's relations

---
+++ src/User.php
@@ -0,0 +1,62 @@
+<?php
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * @Entity @Table(name="users")
+ */
+class User
+{
+    /**
+     * @Id @GeneratedValue @Column(type="integer")
+     * @var int
+     */
+    protected $id;
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @OneToMany(targetEntity="Bug", mappedBy="reporter")
+     * @var Bug[] An ArrayCollection of Bug objects.
+     **/
+    protected $reportedBugs;
+    /**
+     * @OneToMany(targetEntity="Bug", mappedBy="engineer")
+     * @var Bug[] An ArrayCollection of Bug objects.
+     **/
+    protected $assignedBugs;
+
+    public function __construct()
+    {
+        $this->reportedBugs = new ArrayCollection();
+        $this->assignedBugs = new ArrayCollection();
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function addReportedBug(Bug $bug)
+    {
+        $this->reportedBugs[] = $bug;
+    }
+
+    public function assignedToBug(Bug $bug)
+    {
+        $this->assignedBugs[] = $bug;
+    }
+}
